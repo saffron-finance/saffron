a0c1387804a02d4b7ab95f70338a0798e63948f4
Turpintinz <turpintinz@protonmail.com>
2020-12-05 16:37:11 -0700

    Extract Redeem events with Nodejs application

---
+++ scripts/epoch-1-recovery/rSFI/get_redeem_distributions.js
@@ -0,0 +1,67 @@
+const { Command } = require('commander');
+const Web3 = require("web3");
+const fs = require('fs');
+
+const program = new Command();
+program.version('1.0.0');
+program
+    .option('-f, --fromblock <block number>', 'Block number to start reading events')
+    .option('-t, --toblock <block number>', 'Block number to read events to');
+program.parse(process.argv);
+
+
+const fromBlock = program.fromblock ? program.fromblock : 11374859
+const toBlock = program.toblock ? program.toblock : 11394311
+
+console.log(`Reading from block, ${fromBlock}, to block, ${toBlock}.`);
+
+const web3 = new Web3("wss://mainnet.infura.io/ws/v3/eac59ac8301e429aa7d149719e38544c");
+const contractAbi = [{"inputs":[],"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":false,"internalType":"address","name":"sender","type":"address"},{"indexed":false,"internalType":"uint256","name":"balance","type":"uint256"},{"indexed":false,"internalType":"address","name":"token","type":"address"},{"indexed":false,"internalType":"uint256","name":"share","type":"uint256"}],"name":"Redeem","type":"event"},{"inputs":[],"name":"DAI","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"S_INTEREST_EARNED","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"allowRedeem","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"depositFund","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"deposit_done","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"disallowRedeem","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"_token","type":"address"},{"internalType":"address","name":"_to","type":"address"}],"name":"erc_sweep","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"fund_rescue","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"governance","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"lp_token","outputs":[{"internalType":"contract IERC20","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"lp_token_address","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"redeem","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"redeem_allowed","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"_fund_rescue","type":"address"}],"name":"setFundRescue","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"total_distribution_amount","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"underlying_token","outputs":[{"internalType":"contract IERC20","name":"","type":"address"}],"stateMutability":"view","type":"function"}];
+
+let addresses = {
+    '0xFdbd256C0736d79E0082e9a4055A971cb12A296b': 'DistributionSInterest',
+    '0xf65B60d127671ACec535c359640ad390C2411577': 'DistributionSPrincipal',
+    '0x3Eef976C092271bF557cb3Db26420aCdD25B8449': 'DistributionAInterest',
+    '0xb6f4d51F72279e353432fF8E47BB3904e506A403': 'DistributionAPrincipal'
+}
+
+async function retrieveEvents(contractAddress) {
+    const contract = new web3.eth.Contract(contractAbi, contractAddress);
+    return contract.getPastEvents('Redeem',
+        {
+            fromBlock: fromBlock,
+            toBlock: toBlock
+        });
+}
+
+async function processItem(item) {
+    return {
+        sender: item.returnValues.sender,
+        balance: item.returnValues.balance,
+        token: item.returnValues.token,
+        share: item.returnValues.share,
+        transactionHash: item.transactionHash
+    }
+}
+
+async function writeRedeemEvents() {
+    for (const addressesKey in addresses) {
+        let redeemEvents = await retrieveEvents(addressesKey);
+        console.log(`${redeemEvents.length} Redeem events for contract, ${addresses[addressesKey]}.`);
+        let redeemList = [];
+        for (const event of redeemEvents) {
+            let processedItem = await processItem(event);
+            redeemList.push(processedItem);
+        }
+
+        let logName = `${addressesKey.substring(0,8)}-${addresses[addressesKey]}-log.json`;
+        fs.writeFile(logName, JSON.stringify(redeemList, null, 2), (err) => {
+            if (err) throw err;
+            console.log(`Completed writing to ${logName}`);
+        });
+    }
+}
+
+writeRedeemEvents().then(() => {
+    console.log("done")
+})
